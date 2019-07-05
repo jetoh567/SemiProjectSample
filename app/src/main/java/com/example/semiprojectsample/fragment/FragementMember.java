008877a7e373677d5492cc13f4a1ee7c80390be9
LJW <jetoh567@naver.com>
2019-07-05 10:58:55 +0900

    sampler

---
+++ app/src/main/java/com/example/semiprojectsample/fragment/FragementMember.java
@@ -0,0 +1,19 @@
+package com.example.semiprojectsample.fragment;
+
+//Ctrl+Alt+o 누르면 필요한 import만 남길 수 있음
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragementMember extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
